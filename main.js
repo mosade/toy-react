38b653cd86b4c0b8f6ce551befc9402424da485e
pzl <panzhilin1127@gmail.com>
2020-08-24 22:52:28 +0800

    add component support

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-import { createElement } from './toy-react';
-// let a=<div id="a" className="ae">
-//     <h1>1111</h1>
-//     2222
-//     <div>
-//         <p>3333</p>
-//     </div>
-//     <div>4444</div>
-// </div>
-window.a=<div id="a" class="ae">
-    <div>
-        <h1>234</h1>
-        <div><p>456</p></div>
+import { createElement ,Component} from './toy-react';
+class MyComponent extends Component{
+    render(){
+        return <div>
+            {this.children}
         </div>
-    </div>;
-document.body.appendChild(window.a);
+    }
+}
+// let a=<div>123</div>;
+let b=<MyComponent id='mc' class='mce'>
+    <div>123</div>
+    <div>456</div>
+</MyComponent>
+
+document.body.appendChild(b.root);
 
--- toy-react.js
+++ toy-react.js
@@ -1,15 +1,63 @@
-export function createElement(tagName, attributes, ...children) {
-    const el = document.createElement(tagName);
+export class ElementWrapper {
+    constructor(type) {
+        this.root = document.createElement(type);
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+    appendChild(child) {
+        this.root.appendChild(child.root);
+    }
+}
+export class TextNodeWrapper {
+    constructor(type) {
+        this.root = document.createTextNode(type);
+    }
+}
+export class Component {
+    constructor() {
+        this.children = [];
+        this.props = Object.create(null);
+        this._root = null;
+    }
+    setAttribute(name, value) {
+        this.props[name] = value;
+    }
+    appendChild(child) {
+        this.children.push(child);
+    }
+    get root() {
+        if (!this._root) {
+            this._root = this.render().root;
+        }
+        return this._root;
+    }
+    render() {
+    }
+
+}
+export function createElement(type, attributes, ...children) {
+    let el;
+    if (typeof type === 'string') {
+        el = new ElementWrapper(type);
+    } else {
+        el = new type;
+    }
     for (const attr in attributes) {
         el.setAttribute(attr, attributes[attr]);
     }
-    console.log(children);
-    for (const child of children) {
-        if (typeof child === 'string') {
-            el.appendChild(document.createTextNode(child));
-        }else{
-            el.appendChild(child);
+    const insertChildren = (children) => {
+        for (const child of children) {
+            if (Array.isArray(child)) {
+                insertChildren(child);
+            }
+            else if (typeof child === 'string') {
+                el.appendChild(new TextNodeWrapper(child));
+            } else {
+                el.appendChild(child);
+            }
         }
     }
+    insertChildren(children);
     return el;
 }
